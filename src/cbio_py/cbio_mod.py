6f83354e1db3a4a63746971e9cd6613e957b89b9
Sasha Dagayev <odagayev@gmail.com>
2021-12-30 17:35:07 -0500

    working cbio methods

--- src/cbio_py/cbio_mod.py
+++ src/cbio_py/cbio_mod.py
@@ -1,4 +1,26 @@
-#this is a repo for cbio_mod
+from bravado.client import SwaggerClient as bravado_client
 
-def add_one(number):
-    return number + 1
+# This is the initialization of many of the data sources that we will need
+cbioportal = bravado_client.from_url('https://www.cbioportal.org/api/api-docs', 
+    config={"validate_requests":False,"validate_responses":False,"validate_swagger_spec": False,})
+
+#Fetch all of the studies
+def getALLStudies():
+    studies = cbioportal.Studies.getAllStudiesUsingGET().result()
+    return studies
+
+def getALLCancerTypes():
+    cancerTypes = cbioportal.Cancer_Types.getAllCancerTypesUsingGET().result()
+    return cancerTypes
+
+def getCancerByID(id):
+    cancerType = cbioportal.Cancer_Types.getCancerTypeUsingGET(cancerTypeId=id).result()
+    return cancerType
+
+def getALLClinicalAttributes():
+    clinicalAttributes = cbioportal.Clinical_Attributes.getAllClinicalAttributesUsingGET().result()
+    return clinicalAttributes
+
+def getClinicalAttributesByStudyId(id):
+    clinicalAttributes = cbioportal.Clinical_Attributes.getAllClinicalAttributesInStudyUsingGET(studyId=id).result()
+    return clinicalAttributes
