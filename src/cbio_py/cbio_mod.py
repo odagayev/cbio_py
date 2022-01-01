4e226068251e2fe7cdf7c4ee2a7802054d6af26f
Sasha Dagayev <odagayev@gmail.com>
2022-01-01 10:07:15 -0500

    added clininical endpoints

--- src/cbio_py/cbio_mod.py
+++ src/cbio_py/cbio_mod.py
@@ -5,27 +5,39 @@ cbioportal = bravado_client.from_url('https://www.cbioportal.org/api/api-docs',
     config={"validate_requests":False,"validate_responses":False,"validate_swagger_spec": False,})
 
 #Fetch all of the studies
-def getALLStudies():
+def getAllStudies():
     studies = cbioportal.Studies.getAllStudiesUsingGET().result()
     return studies
 
-def getALLCancerTypes():
+def getAllCancerTypes():
     cancerTypes = cbioportal.Cancer_Types.getAllCancerTypesUsingGET().result()
     return cancerTypes
 
-def getCancerByID(id):
-    cancerType = cbioportal.Cancer_Types.getCancerTypeUsingGET(cancerTypeId=id).result()
+def getCancerByID(cancerTypeId):
+    cancerType = cbioportal.Cancer_Types.getCancerTypeUsingGET(cancerTypeId=cancerTypeId).result()
     return cancerType
 
-def getALLClinicalAttributes():
+def getAllClinicalAttributes():
     clinicalAttributes = cbioportal.Clinical_Attributes.getAllClinicalAttributesUsingGET().result()
     return clinicalAttributes
 
-def getClinicalAttributesByStudyId(id):
-    clinicalAttributes = cbioportal.Clinical_Attributes.getAllClinicalAttributesInStudyUsingGET(studyId=id).result()
+def getClinicalAttributesByStudyId(studyId):
+    clinicalAttributes = cbioportal.Clinical_Attributes.getAllClinicalAttributesInStudyUsingGET(studyId=studyId).result()
     return clinicalAttributes
 
 def getClinicalAttributeInStudy(studyId, clinicalAttributeId):
     clinicalAttributes = cbioportal.Clinical_Attributes.getClinicalAttributeInStudyUsingGET(clinicalAttributeId=clinicalAttributeId,studyId=studyId).result()
     return clinicalAttributes
+
+def getAllClinicalDataInStudy(studyId):
+    clinicalData = cbioportal.Clinical_Data.getAllClinicalDataInStudyUsingGET(studyId=studyId).result()
+    return clinicalData
+
+def getAllClinicalDataOfPatientInStudy(studyId, patientId):
+    clinicalData = cbioportal.Clinical_Data.getAllClinicalDataOfPatientInStudyUsingGET(studyId=studyId,patientId=patientId).result()
+    return clinicalData
+
+def getAllClinicalDataOfSampleInStudy(studyId, sampleId):
+    clinicalData = cbioportal.Clinical_Data.getAllClinicalDataOfSampleInStudyUsingGET(studyId=studyId,sampleId=sampleId).result()
+    return clinicalData
 
